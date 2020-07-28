1afb077aca30122c5b65aa425d9d81416231ae4a
Marques Avery <averymarques4@gmail.com>
2020-07-28 17:27:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -111,19 +111,21 @@ end
 
 
 def winner(board)
-  if over?(board) == true
-    WIN_COMBINATIONS.each do |win_combination|
-      win_index_1 = win_combination[0]
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
-
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
-      if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        return "X"
-      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-        return "O"
+  binding.pry
+  won?(board)
+  # if over?(board)
+    # WIN_COMBINATIONS.each do |win_combination|
+    #   win_index_1 = win_combination[0]
+    #   win_index_2 = win_combination[1]
+    #   win_index_3 = win_combination[2]
+    #
+    #   position_1 = board[win_index_1]
+    #   position_2 = board[win_index_2]
+    #   position_3 = board[win_index_3]
+      # if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      #   return "X"
+      # elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      #   return "O"
       end
     end
   end
